970d468c11af20561ab9184fd91d2d4aacd323e3
Pierre Guyard <pguyard59@gmail.com>
2024-09-30 18:21:26 +0200

    Modification par rapport au format de la date de transaction

--- bankAccount/src/main/kotlin/com/example/bankAccount/adapters/repository/SpringDataAccountRepositoryAdapter.kt
+++ bankAccount/src/main/kotlin/com/example/bankAccount/adapters/repository/SpringDataAccountRepositoryAdapter.kt
@@ -6,10 +6,6 @@ import com.example.bankAccount.domain.Account
 import com.example.bankAccount.domain.AccountRepository
 import com.example.bankAccount.domain.Transaction
 import org.springframework.stereotype.Repository
-import java.time.LocalDateTime
-import java.time.ZoneId
-import java.time.ZoneOffset
-import java.time.format.DateTimeFormatter
 
 @Repository
 open class SpringDataAccountRepositoryAdapter(private val springDataAccountRepository: SpringDataAccountRepository) :
@@ -87,20 +83,9 @@ private fun TransactionEntity.toDomain(): Transaction {
  */
 private fun Transaction.toEntity(account: Account): TransactionEntity {
 
-    // Creer un formateur de date avec le format souhaité
-    val dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
-
-    // Utiliser le fuseau horaire du système
-    val formatter = dateTimeFormatter.withZone(ZoneId.systemDefault())
-
-    // Formatter la date formattee en Instant
-    val formattedDate = formatter.format(this.date)
-    val localDateTime = LocalDateTime.parse(formattedDate, dateTimeFormatter)
-    val instantFormatted = localDateTime.toInstant(ZoneOffset.UTC)
-
     return TransactionEntity(
         id = this.id,
-        date = instantFormatted,
+        date = this.date,
         operation = this.operation,
         amount = this.amount,
         accountIban = account.iban
